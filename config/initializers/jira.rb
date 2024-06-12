62e6af7a89677543b445d1474fd4911142d9bfb9
Alamin Biswas Plabon <72992978+Alaminplabon@users.noreply.github.com>
2024-06-12 21:11:55 +0600

    Update jira.rb

--- config/initializers/jira.rb
+++ config/initializers/jira.rb
@@ -3,5 +3,5 @@ JIRA_CLIENT = JIRA::Client.new(
   context_path: '',
   auth_type: :basic,
   username: 'alaminbiswasplabon98@gmail.com',  # Set this in your environment variables
-  password: 'ATATT3xFfGF0mMj_hgwpBd3dCZ4747ODrd5iDO_1D2zPFcUaopISEiFG_ARuVORm07qgLfxUC8J25HPNc-k49jwSsz2zm06NlwCXmMSX6pY8Kj6F8wFIxO7jHu5kQ37B2h2lHZh3X5tvleFJN327GVZZjhJZGF7Vdg1m3dt3PLmimAhbqmIwV6Y=B3C57BCD'  # Set this in your environment variables
+  password: 'ATATT3xFfGF0xeyz20oTWaWjNUr1LIYO98j6OTQtkVYzaaT5LEbBDNHiZLs7Z2P1z_W8IoWRC7_5BLcZfTkZgrAeJE-NOdaLvtD7Zcg2CxR0il_QDWGmbWXJ6nO0z74y0J8Q8DbG66q6AOwvdH6HCERreM6sts0LbukhVkio8SybzaMGILDtPjg=B720C000'  # Set this in your environment variables
 )
